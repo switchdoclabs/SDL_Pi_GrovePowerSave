9fb2578626caa74ff766053116e1d854b7a293e4
John Shovic <jshovic@switchdoc.com>
2017-05-01 16:25:03 -0700

    Initial Commit

---
+++ SDL_Pi_GrovePowerSave.py
@@ -0,0 +1,35 @@
+#
+#
+# SDL_Pi_GrovePowerSave
+# Raspberry Pi Driver for the SwitchDoc Labs GrovePowerSave 
+#
+# SwitchDoc Labs
+# April  2017
+#
+# Version 1.1
+
+import RPi.GPIO as GPIO
+
+GrovePowerSave_Default_GPIO_Pin = 20
+
+class SDL_Pi_GrovePowerSave:
+	
+	def __init__(self, GPIOPin=GrovePowerSave_Default_GPIO_Pin, initialState = True):
+                self._GPIOPin = GPIOPin
+                self._initialState = initialState
+		
+		GPIO.setmode(GPIO.BCM)
+
+		GPIO.setup(self._GPIOPin,GPIO.OUT, initial=self._initialState)
+
+	def turnOnPowerSave(self):
+
+		GPIO.output(self._GPIOPin, True)
+
+	def turnOffPowerSave(self):
+
+		GPIO.output(self._GPIOPin, False)
+
+
+		
+		
